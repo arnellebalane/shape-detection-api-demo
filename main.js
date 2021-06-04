93d50131ff652b8d92fa57623ad714488efbfd57
Arnelle Balane <arnellebalane@gmail.com>
2021-06-04 21:54:28 +0800

    Cleanup source code

--- main.js
+++ main.js
@@ -2,116 +2,116 @@ const canvas = document.querySelector('canvas');
 const context = canvas.getContext('2d');
 
 if (isShapeDetectionApiSupported()) {
-    runShapeDetectionApiDemo();
+  runShapeDetectionApiDemo();
 } else {
-    displayFallbackMessage();
+  displayFallbackMessage();
 }
 
 function isShapeDetectionApiSupported() {
-    return window.FaceDetector && window.TextDetector && window.BarcodeDetector;
+  return window.FaceDetector && window.TextDetector && window.BarcodeDetector;
 }
 
 async function runShapeDetectionApiDemo() {
-    const constraints = { video: { facingMode: 'environment' } };
-    const mediaStream = await navigator.mediaDevices.getUserMedia(constraints);
+  const constraints = { video: { facingMode: 'environment' } };
+  const mediaStream = await navigator.mediaDevices.getUserMedia(constraints);
 
-    const video = document.createElement('video');
-    video.srcObject = mediaStream;
-    video.autoplay = true;
-    video.onloadedmetadata = () => {
-        canvas.width = video.videoWidth;
-        canvas.height = video.videoHeight;
-    };
+  const video = document.createElement('video');
+  video.srcObject = mediaStream;
+  video.autoplay = true;
+  video.onloadedmetadata = () => {
+    canvas.width = video.videoWidth;
+    canvas.height = video.videoHeight;
+  };
 
-    let renderLocked = false;
-    const faceDetector = new FaceDetector({ fastMode: true });
-    const textDetector = new TextDetector();
-    const barcodeDetector = new BarcodeDetector();
+  let renderLocked = false;
+  const faceDetector = new FaceDetector({ fastMode: true });
+  const textDetector = new TextDetector();
+  const barcodeDetector = new BarcodeDetector();
 
-    function render() {
-        if (!video.paused) {
-            renderLocked = true;
+  function render() {
+    if (!video.paused) {
+      renderLocked = true;
 
-            Promise.all([
-                faceDetector.detect(video).catch((error) => console.error(error)),
-                textDetector.detect(video).catch((error) => console.error(error)),
-                barcodeDetector.detect(video).catch((error) => console.log(error))
-            ]).then(([detectedFaces = [], detectedTexts = [], detectedBarcodes = []]) => {
-                context.clearRect(0, 0, canvas.width, canvas.height);
-                context.drawImage(video, 0, 0, video.videoWidth, video.videoHeight);
+      Promise.all([
+        faceDetector.detect(video).catch((error) => console.error(error)),
+        textDetector.detect(video).catch((error) => console.error(error)),
+        barcodeDetector.detect(video).catch((error) => console.log(error)),
+      ]).then(([detectedFaces = [], detectedTexts = [], detectedBarcodes = []]) => {
+        context.clearRect(0, 0, canvas.width, canvas.height);
+        context.drawImage(video, 0, 0, video.videoWidth, video.videoHeight);
 
-                context.strokeStyle = '#ffeb3b';
-                context.fillStyle = '#ffeb3b';
-                context.font = '16px Mononoki';
-                context.lineWidth = 5;
+        context.strokeStyle = '#ffeb3b';
+        context.fillStyle = '#ffeb3b';
+        context.font = '16px monospace';
+        context.lineWidth = 5;
 
-                detectedFaces.forEach((detectedFace) => {
-                    const { top, left, width, height } = detectedFace.boundingBox;
-                    context.beginPath();
-                    context.rect(left, top, width, height);
-                    context.stroke();
-                    context.fillText('face detected', left + 5, top - 8);
+        detectedFaces.forEach((detectedFace) => {
+          const { top, left, width, height } = detectedFace.boundingBox;
+          context.beginPath();
+          context.rect(left, top, width, height);
+          context.stroke();
+          context.fillText('face detected', left + 5, top - 8);
 
-                    if (detectedFace.landmarks) {
-                        detectedFace.landmarks.forEach((landmark) => {
-                            context.beginPath();
-                            context.arc(landmark.locations.x, landmark.locations.y, 5, 0, Math.PI * 2);
-                            context.fill();
-                            context.fillText(landmark.type, landmark.locations.x + 10, landmark.locations.y + 4);
-                        });
-                    }
-                });
-
-                context.strokeStyle = '#f44336';
-                context.fillStyle = '#f44336';
-                context.font = '24px Mononoki';
-
-                detectedTexts.forEach((detectedText) => {
-                    const { top, left, width, height } = detectedText.boundingBox;
-                    context.beginPath();
-                    context.rect(left, top, width, height);
-                    context.stroke();
-                    context.fillText(detectedText.rawValue, left + 5, top - 12);
-                });
-
-                context.strokeStyle = '#03A9F4';
-                context.fillStyle = '#03A9F4';
-                context.font = '16px Mononoki';
-
-                detectedBarcodes.forEach((detectedBarcode) => {
-                    const { top, left, width, height } = detectedBarcode.boundingBox;
-                    const cornerPoints = detectedBarcode.cornerPoints;
-                    if (cornerPoints && cornerPoints.length) {
-                        const [{ x, y }] = cornerPoints;
-                        context.beginPath();
-                        context.moveTo(x, y);
-                        for (let i = 1; i < cornerPoints.length; i++) {
-                            context.lineTo(cornerPoints[i].x, cornerPoints[i].y);
-                        }
-                        context.closePath();
-                    } else {
-                        context.beginPath();
-                        context.rect(left, top, width, height);
-                    }
-                    context.stroke();
-                    context.fillText(detectedBarcode.rawValue, left, top + height + 16);
-                });
-
-                renderLocked = false;
+          if (detectedFace.landmarks) {
+            detectedFace.landmarks.forEach((landmark) => {
+              context.beginPath();
+              context.arc(landmark.locations.x, landmark.locations.y, 5, 0, Math.PI * 2);
+              context.fill();
+              context.fillText(landmark.type, landmark.locations.x + 10, landmark.locations.y + 4);
             });
-        }
-    }
+          }
+        });
 
-    (function renderLoop() {
-        requestAnimationFrame(renderLoop);
-        if (!renderLocked) {
-            render();
-        }
-    })();
-};
+        context.strokeStyle = '#f44336';
+        context.fillStyle = '#f44336';
+        context.font = '24px monospace';
+
+        detectedTexts.forEach((detectedText) => {
+          const { top, left, width, height } = detectedText.boundingBox;
+          context.beginPath();
+          context.rect(left, top, width, height);
+          context.stroke();
+          context.fillText(detectedText.rawValue, left + 5, top - 12);
+        });
+
+        context.strokeStyle = '#03A9F4';
+        context.fillStyle = '#03A9F4';
+        context.font = '16px monospace';
+
+        detectedBarcodes.forEach((detectedBarcode) => {
+          const { top, left, width, height } = detectedBarcode.boundingBox;
+          const cornerPoints = detectedBarcode.cornerPoints;
+          if (cornerPoints && cornerPoints.length) {
+            const [{ x, y }] = cornerPoints;
+            context.beginPath();
+            context.moveTo(x, y);
+            for (let i = 1; i < cornerPoints.length; i++) {
+              context.lineTo(cornerPoints[i].x, cornerPoints[i].y);
+            }
+            context.closePath();
+          } else {
+            context.beginPath();
+            context.rect(left, top, width, height);
+          }
+          context.stroke();
+          context.fillText(detectedBarcode.rawValue, left, top + height + 16);
+        });
+
+        renderLocked = false;
+      });
+    }
+  }
+
+  (function renderLoop() {
+    requestAnimationFrame(renderLoop);
+    if (!renderLocked) {
+      render();
+    }
+  })();
+}
 
 function displayFallbackMessage() {
-    document.querySelector('.fallback-message').classList.remove('hidden');
-    document.querySelector('canvas').classList.add('hidden');
-    document.querySelector('.links').classList.add('hidden');
+  document.querySelector('.fallback-message').classList.remove('hidden');
+  document.querySelector('canvas').classList.add('hidden');
+  document.querySelector('.links').classList.add('hidden');
 }
